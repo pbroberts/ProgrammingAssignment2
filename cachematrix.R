a46cb1c5c2b1dbc4c990d730ebee5de0eca98015
pbroberts <robertspb@yahoo.com>
2015-03-20 22:55:01 -0500

    fixed for n dim matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,96 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions take an input matrix, solves the matrix inverse,
+## and caches the inverse for quick retrieval later; they assume an 
+## invertible matrix is provided as input
 
-## Write a short comment describing this function
-
-#makeVector <- function(x = numeric()) {
-makeCacheMatrix <- function(x = matrix()) {
-  
-    #m <- NULL
-    storedSolvedDataMatrix <- NULL
-    
-    #set <- function(y) {
-    set <- function(y = numeric()) {  
-            #x <<- y
-            dataMatrix <<- y
-            #m <<- NULL
-            storedSolvedDataMatrix <<- NULL
-    }
-    
-    #get <- function() x
-    get <- function() {
-            return (dataMatrix)
-    }
-    
-    #setmean <- function(mean) m <<- mean
-    setSolvedDataMatrix <- function(sentSolvedDataMatrix) {
-            storedSolvedDataMatrix <<- sentSolvedDataMatrix
-    }
-    
-    #getmean <- function() m
-    getSolvedDataMatrix <- function() {
-            storedSolvedDataMatrix
-    }
-    
-    #list(set = set, get = get, setmean = setmean, getmean = getmean)
-    list(set = set, 
-         get = get, 
-         setSolvedDataMatrix = setSolvedDataMatrix, 
-         getSolvedDataMatrix = getSolvedDataMatrix)
+## converts the matrix into an object which support caching
+## takes a matrix as input
+makeCacheMatrix <- function(x=NULL) { 
+        
+        #set initial to null
+        cachedInverse <- NULL #matrix()
+        
+        ## provide a method to set the cached solved matrix 
+        ## in the parent environment
+        setMatrix <- function(y) {
+                x <<- y
+                cachedInverse <<- NULL #matrix()
+        }
+        
+        getMatrix <- function() x
+        setInverse <- function(passedInSlvdMtrx) {
+                cachedInverse <<- passedInSlvdMtrx
+        }
+        
+        # provide a method to retrieve the cached solved matrix
+        getInverse <- function() cachedInverse 
+        
+        #return the get and set functions 
+        list(getMatrix = getMatrix,
+             setMatrix = setMatrix, 
+             setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(makeCacheMatrix, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        localInverse <- makeCacheMatrix$getSolvedDataMatrix
+## takes a makeCacheMatrix object input, creates and stores inverse on first call
+## returns cached inverse on subsequent calls
+cacheSolve <- function(x, ...) {
         
-        if(!is.null(localInverse)) {
-                message("No previously cached inverse")
-                return(localInverse)
-        }
+        # set local variable to cached inverse
+        cachedInverse <- x$getInverse()
+        message("Checking cache for inverse...")
+        
+        # test to see if cached inverse exists 
+        if(!is.null(cachedInverse)) {
+                message("getting cached data")
+                }
+        # if cache doesn't exist, create it
         else {
-                localMatrix <- makeCacheMatrix$get()
-                localInverse <- solve(localMatrix, ...)
-                
-                makeCacheMatrix$setSolvedDataMatrix(localInverse)
-                return(localInverse)
-        }
-}
-makeCacheMatrix2 <- function(x=matrix(,nrow=2,ncol=2)) {
-        
-        #m <- NULL
-        storedSolvedDataMatrix <- x
-        print(storedSolvedDataMatrix)
-
-        #setmean <- function(mean) m <<- mean
-        setfunction <- function() storedSolvedDataMatrix 
-        
-        #getmean <- function() m
-        getfunction <- function() {
-                storedSolvedDataMatrix <- storedSolvedDataMatrix
+                message("creating new cache...")
+                data <-x$getMatrix()
+                cachedInverse <- solve(data)
+                x$setInverse(cachedInverse)
         }
         
-        #list(set = set, get = get, setmean = setmean, getmean = getmean)
-        list(set = setfunction, 
-             get = getfunction)
-}
-
-cacheSolve2 <- function(makeCacheMatrix2, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        localInverse <- makeCacheMatrix2$get
-        
-        if(!is.null(localInverse)) {
-                message("No previously cached inverse")
-                #return(localInverse)
-        }
-        else {
-                print(localInverse)
-        }
-        message("Setting new cached value...")
-        localMatrix <- makeCacheMatrix2$set()
-        localInverse <- solve(localMatrix, ...)
-        print(makeCacheMatrix2$get())        
-
+        print(cachedInverse)
 }
